cd870db8b78026b8b5ca410eab6b1ff9e4898b91
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2023-03-30 17:17:47 -0400

    feat: added addons store management

---
+++ src/extension/utils/storage/addons/index.ts
@@ -0,0 +1,33 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  CollectionReference,
+  DocumentData,
+} from 'firebase/firestore';
+
+import { firestore } from '../firestore';
+
+export type Addon = {
+  url: string;
+};
+
+export class Addons {
+  private _db: CollectionReference<DocumentData>;
+
+  constructor() {
+    this._db = collection(firestore, 'addons');
+  }
+
+  public async exists(id: string) {
+    const document = await getDoc(doc(this._db, id));
+
+    return document.exists();
+  }
+
+  public async get(id: string): Promise<Addon | undefined> {
+    const document = await getDoc(doc(this._db, id));
+
+    return document.data() as Addon;
+  }
+}
---
+++ src/extension/utils/storage/users/addons/index.ts
@@ -0,0 +1,30 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  CollectionReference,
+  DocumentData,
+} from 'firebase/firestore';
+
+import { firestore } from '../../firestore';
+
+export type UserAddon = Map<
+  string,
+  {
+    enabled: boolean;
+  }
+>;
+
+export class UserAddons {
+  private _db: CollectionReference<DocumentData>;
+
+  constructor() {
+    this._db = collection(firestore, 'users');
+  }
+
+  public async list(uid: string): Promise<UserAddon | undefined> {
+    const document = await getDoc(doc(this._db, uid, 'addons'));
+
+    return document.data() as UserAddon;
+  }
+}
