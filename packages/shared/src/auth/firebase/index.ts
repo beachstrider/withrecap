4013b33f4aaebfb7ee3e105357cfd78a38b2b96a
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2023-04-07 13:12:39 -0400

    fix(firebase): add measurementId config key

--- packages/shared/src/auth/firebase/index.ts
+++ packages/shared/src/auth/firebase/index.ts
@@ -8,7 +8,8 @@ const firebaseConfig: FirebaseOptions = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 export const firebase = initializeApp(firebaseConfig)
--- packages/shared/src/typings.d.ts
+++ packages/shared/src/typings.d.ts
@@ -1,10 +1,11 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    FIREBASE_API_KEY: string
-    FIREBASE_AUTH_DOMAIN: string
-    FIREBASE_PROJECT_ID: string
-    FIREBASE_STORAGE_BUCKET: string
-    FIREBASE_MESSAGING_SENDER_ID: string
-    FIREBASE_APP_ID: string
+    FIREBASE_API_KEY?: string
+    FIREBASE_AUTH_DOMAIN?: string
+    FIREBASE_PROJECT_ID?: string
+    FIREBASE_STORAGE_BUCKET?: string
+    FIREBASE_MESSAGING_SENDER_ID?: string
+    FIREBASE_APP_ID?: string
+    FIREBASE_MEASUREMENT_ID?: string
   }
 }
