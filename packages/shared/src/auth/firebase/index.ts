243ceecd03d1c362ff75644abc3ef215fa31475f
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2023-04-07 13:01:34 -0400

    chore: prepare extension for testing

--- packages/shared/src/auth/firebase/index.ts
+++ packages/shared/src/auth/firebase/index.ts
@@ -1,14 +1,14 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app'
+
 import 'firebase/compat/auth'
 
-// TODO: Put this in env
 const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyD4E94nsyAL--9HTl8mdey5vNM5g41Sqds',
-  authDomain: 'recap-dev-3c341.firebaseapp.com',
-  projectId: 'recap-dev-3c341',
-  storageBucket: 'recap-dev-3c341.appspot.com',
-  messagingSenderId: '920065143357',
-  appId: '1:920065143357:web:d6bf33c14c8cd073cbb054'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 export const firebase = initializeApp(firebaseConfig)
---
+++ packages/shared/src/typings.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    FIREBASE_API_KEY: string
+    FIREBASE_AUTH_DOMAIN: string
+    FIREBASE_PROJECT_ID: string
+    FIREBASE_STORAGE_BUCKET: string
+    FIREBASE_MESSAGING_SENDER_ID: string
+    FIREBASE_APP_ID: string
+  }
+}
