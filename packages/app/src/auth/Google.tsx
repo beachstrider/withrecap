f982407dcb5d7445b843e92c7b14bbeb36443efc
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2023-04-13 13:37:03 -0400

    fix(app): add scope to google auth provider

--- packages/app/src/auth/Google.tsx
+++ packages/app/src/auth/Google.tsx
@@ -3,6 +3,11 @@ import { auth } from '../firebase'
 
 const provider = new GoogleAuthProvider()
 
+// Add scope to read signed in user info
+provider.addScope('https://www.googleapis.com/auth/userinfo.email')
+// Add scope to read user calendar events
+provider.addScope('https://www.googleapis.com/auth/calendar.events.readonly')
+
 export function signin() {
   signInWithRedirect(auth, provider)
 }
