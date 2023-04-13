822e15485df713a19690308e3db544e08df037e3
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2023-04-13 15:03:19 -0400

    fix: tell tailwind to look at files in shared also

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts,tsx}'],
+  content: ['./src/**/*.{html,ts,tsx}', '../shared/src/**/*.{html,tsx}'],
   theme: {
     container: {
       center: true
