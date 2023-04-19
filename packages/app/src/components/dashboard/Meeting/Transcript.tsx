7a7f6cb92bb2c9b3af46fe4235afaf3f6d821419
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-20 05:35:54 +0900

    fix(app): avatar ratio issue

--- packages/app/src/components/dashboard/Meeting/Transcript.tsx
+++ packages/app/src/components/dashboard/Meeting/Transcript.tsx
@@ -44,7 +44,11 @@ const TranscriptItem = ({ msg }: { msg: Message }) => {
   return (
     <div className="flex sm:gap-[16px] gap-[12px]">
       {/* TODO: avatar will be set by choosing one in attendees later once it has proper identify (email) */}
-      <UserAvatar name={msg.speaker} avatar={undefined} className="sm:w-[24px] sm:h-[24px] w-[18px] h-[18px]" />
+      <UserAvatar
+        name={msg.speaker}
+        avatar={undefined}
+        className="sm:w-[24px] sm:h-[24px] w-[18px] h-[18px] sm:text-[10px] text-[7px]"
+      />
       <div className="flex flex-col sm:gap-[10px] gap-[6px]">
         <div className="font-semibold">{msg.speaker}</div>
         <div className="text-gray-500">{msg.text}</div>
