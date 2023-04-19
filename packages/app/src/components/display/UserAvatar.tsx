9c772e76266d98bd19c7ca7bb6f0d6dd7bed0a04
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-20 03:41:03 +0900

    fix(app): add UserAvatar component

---
+++ packages/app/src/components/display/UserAvatar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+  className?: string
+  name?: string
+  avatar?: string
+}
+
+export default function UserAvatar({ className, name, avatar }: Props) {
+  return (
+    <div className={`flex w-[28px] h-[28px] rounded-full overflow-hidden ${className}`}>
+      {avatar && <img src={avatar} alt="" className="flex-1" />}
+      {!avatar && (
+        <div className="flex items-center justify-center flex-1 font-semibold bg-gray-200 sm:text-[12px] text-[9px]">
+          {typeof name !== 'undefined' ? name.substring(0, 1) : '?'}
+        </div>
+      )}
+    </div>
+  )
+}
