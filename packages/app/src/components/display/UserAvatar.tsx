b466ef4da94a23bc392fc5286082925711a1282c
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-20 05:35:54 +0900

    fix(app): avatar ratio issue

--- packages/app/src/components/display/UserAvatar.tsx
+++ packages/app/src/components/display/UserAvatar.tsx
@@ -8,10 +8,12 @@ interface Props {
 
 export default function UserAvatar({ className, name, avatar }: Props) {
   return (
-    <div className={`flex w-[28px] h-[28px] shrink-0 rounded-full overflow-hidden ${className}`}>
+    <div
+      className={`flex w-[28px] h-[28px] shrink-0 rounded-full overflow-hidden sm:text-[12px] text-[9px] ${className}`}
+    >
       {avatar && <img src={avatar} alt="" className="flex-1" />}
       {!avatar && (
-        <div className="flex items-center justify-center flex-1 font-semibold bg-gray-200 sm:text-[12px] text-[9px]">
+        <div className="flex items-center justify-center flex-1 font-semibold bg-gray-200">
           {name?.substring(0, 1) || '?'}
         </div>
       )}
