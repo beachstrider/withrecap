01b337c5ef9c4870df83a44ea8fb0ceb64aa6c20
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-21 23:23:08 +0900

    feat(app): add support email popup in terms & conditions, privacy policy

--- packages/app/src/constants/routes.ts
+++ packages/app/src/constants/routes.ts
@@ -12,9 +12,13 @@ export const SIGNING_IN = '#signin' // if firebase is still processing auth stat
 // popup email links
 export const SUPPORT_REQUEST = `mailto:support@withrecap.com?subject=${encodeURIComponent(
   'Request for Assistance'
-)}&body=${encodeURIComponent(``)}
-`
-
+)}&body=${encodeURIComponent(``)}`
+export const SUPPORT_PRIVACY_REQUEST = `mailto:support@withrecap.com?subject=${encodeURIComponent(
+  'Question for Privacy policy'
+)}&body=${encodeURIComponent(``)}`
+export const SUPPORT_TERMS_CONDITIONS_REQUEST = `mailto:support@withrecap.com?subject=${encodeURIComponent(
+  'Question for Terms and Conditions'
+)}&body=${encodeURIComponent(``)}`
 export const DELETE_ACCOUNT_REQUEST = `mailto:support@withrecap.com?subject=${encodeURIComponent(
   'Delete my account'
 )}&body=${encodeURIComponent(``)}`
