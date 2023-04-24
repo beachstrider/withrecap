95fc39ab0a74a52b983c603bcd145d46408c34b0
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-24 21:16:03 +0900

    test(app): prettier

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        {/* Website routes */}
+        {/* Website Routes */}
         <Route path="/">
           <Route
             index
