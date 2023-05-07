0c517e2a3b1acf9e4729391aec0548c6f5685cf3
Jason Mendoza <mernstackdev729@gmail.com>
2023-05-07 12:10:48 +0900

    fix(app): signin button ui for google branching

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 
 import Website from './pages'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Signin from './pages/Signin'
+import TermsConditions from './pages/TermsConditions'
+
 import Integrations from './pages/Integrations'
 import Meetings from './pages/Meetings'
 import MeetingDetails from './pages/Meetings/Details'
-import PrivacyPolicy from './pages/PrivacyPolicy'
 
 import NotFound from './pages/NotFound'
-import TermsConditions from './pages/TermsConditions'
 
 export default function App() {
   const navigate = useNavigate()
@@ -20,7 +22,11 @@ export default function App() {
 
   const loadingComponent = <LoadingScreen />
 
-  const authProvider = <AuthProvider provider={GoogleAuthProvider} />
+  const signin = (
+    <AuthProvider provider={GoogleAuthProvider}>
+      <Signin />
+    </AuthProvider>
+  )
 
   const authGuard = (
     <AuthGuard onNeedAuth={onNeedAuth} provider={GoogleAuthProvider} loadingComponent={loadingComponent} />
@@ -29,12 +35,15 @@ export default function App() {
   return (
     <>
       <Routes>
-        {/* Website Routes */}
-        <Route path="/" element={authProvider}>
+        {/* Website routes */}
+        <Route path="/">
           <Route index element={<Website />} />
           <Route path="privacy-policy" element={<PrivacyPolicy />} />
           <Route path="terms-conditions" element={<TermsConditions />} />
         </Route>
+
+        {/* Auth routes */}
+        <Route path="/signin" element={signin} />
 
         {/* App routes */}
         <Route path="/app/" element={authGuard}>
