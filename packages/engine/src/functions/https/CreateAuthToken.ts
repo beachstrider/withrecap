0da4d63c5ec09e34848b7583b459ab5240887c70
Jason Mendoza <mernstackdev729@gmail.com>
2023-05-10 03:11:21 +0900

    fix(engine): onCall params types in CreateAuthToken

--- packages/engine/src/functions/https/CreateAuthToken.ts
+++ packages/engine/src/functions/https/CreateAuthToken.ts
@@ -3,20 +3,16 @@ import { auth } from '../../config'
 import { SentryWrapper } from '../../utils/sentry'
 
 export const CreateAuthToken = functions.https.onCall(
-  SentryWrapper<[any, functions.https.CallableContext]>(
-    'CreateAuthToken',
-    'functions.https.onCall',
-    async (_, context) => {
-      if (!context.auth) {
-        throw new functions.https.HttpsError('unauthenticated', 'You must be authenticated to use this function')
-      }
-      try {
-        const token = await auth.createCustomToken(context.auth.uid)
-        return { token }
-      } catch (error) {
-        functions.logger.error(error)
-        return { error }
-      }
+  SentryWrapper('CreateAuthToken', 'functions.https.onCall', async (_, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError('unauthenticated', 'You must be authenticated to use this function')
     }
-  )
+    try {
+      const token = await auth.createCustomToken(context.auth.uid)
+      return { token }
+    } catch (error) {
+      functions.logger.error(error)
+      return { error }
+    }
+  })
 )
