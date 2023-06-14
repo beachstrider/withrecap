121484372d8a937e2a302586fbce08dc07615dc9
Jason Mendoza <mernstackdev729@gmail.com>
2023-06-14 05:15:15 -0400

    chore(engine): import UpdateRecording

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,5 +1,8 @@
 export { OnMeetingUpdated } from './functions/background/OnMeetingUpdated'
 export { OnUserCreated } from './functions/background/OnUserCreated'
+
 export { CreateAuthToken } from './functions/callables/CreateAuthToken'
 export { SendInviteEmails } from './functions/callables/SendInviteEmails'
+export { UpdateRecording } from './functions/callables/UpdateRecording'
+
 export { BackupFirestore } from './functions/schedules/Backup'
