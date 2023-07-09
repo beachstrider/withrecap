c7110c85f7f16477c4599746df831f2fa96903ad
Jason Mendoza <mernstackdev729@gmail.com>
2023-07-09 01:47:59 -0400

    fix(seeder): meeting id

--- packages/seeder/src/index.ts
+++ packages/seeder/src/index.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin'
 import { Addons, Meeting, deleteCollection } from '@recap/shared'
 
 import addonsJSON from './data/addons.json'
-import meetingJSON from './data/meeting.json'
+import meetingsJSON from './data/meetings.json'
 
 const app = admin.initializeApp({
   credential: admin.credential.cert('../../service-account.json')
@@ -14,21 +14,24 @@ const db = app.firestore()
 const reset = async () => {
   await deleteCollection(db, 'addons')
   await deleteCollection(db, 'meetings')
+  await deleteCollection(db, 'users')
 }
 
 const seed = async () => {
   const addons: Addons = addonsJSON
-  const meeting: Meeting = meetingJSON
+  const meetings: { [mid: string]: Meeting } = meetingsJSON
 
   // Add addons
-  await db.collection('addons')
-  for await (const key of Object.keys(addons as Addons)) {
-    const addon = addons[key]
-    await db.collection('addons').add(addon)
+  for await (const id of Object.keys(addons)) {
+    const addon = addons[id]
+    await db.collection('addons').doc(id).set(addon)
   }
 
   // Add meeting
-  await db.collection('meetings').add(meeting)
+  for await (const mid of Object.keys(meetings)) {
+    const meeting = meetings[mid]
+    await db.collection('meetings').doc(mid).set(meeting)
+  }
 }
 
 const main = async () => {
