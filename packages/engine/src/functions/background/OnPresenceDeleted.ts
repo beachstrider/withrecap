494337129c5fe03291e2e5a643293d6864a04454
Jason Mendoza <mernstackdev729@gmail.com>
2023-07-18 03:28:50 -0400

    fix(engine): dayjs plugin extend

--- packages/engine/src/functions/background/OnPresenceDeleted.ts
+++ packages/engine/src/functions/background/OnPresenceDeleted.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/node'
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
 import * as functions from 'firebase-functions'
 
 import { Meeting, MeetingMetadata, User } from '@recap/shared'
@@ -16,6 +17,7 @@ import { debug, error, warn } from '../../utils/logger'
 import { SentryWrapper } from '../../utils/sentry'
 
 dayjs.extend(timezone)
+dayjs.extend(utc)
 
 const options: functions.RuntimeOptions = {
   memory: '512MB',
@@ -95,15 +97,16 @@ export const OnPresenceDeleted = functions
           const mail = new MailService(mailgun, settings.domain)
 
           for (const email of emails) {
+            const document = await db.collection('users').where('email', '==', email).limit(1).get()
+
+            let user: User | undefined
+            document.forEach((doc) => (user = doc.data() as User))
+
+            // We currently only send email to users with an account
+            if (!user) continue
+
             try {
-              const document = await db.collection('users').where('email', '==', email).limit(1).get()
-
-              let user: User | undefined
-              document.forEach((doc) => (user = doc.data() as User))
-
-              // We currently only send email to users with an account
-              if (!user) continue
-
+              debug(`infering missing user info...`)
               // HACK: Update the attendee's display name if the matching account has one
               if (user.displayName) meeting.attendees[user.email].name = user.displayName
               if (user.photoURL) meeting.attendees[user.email].avatar = user.photoURL
@@ -118,11 +121,18 @@ export const OnPresenceDeleted = functions
                 })
               }
 
-              debug(`sending email to ${email}...`)
+              debug('user info infered')
+            } catch (err) {
+              error(err)
+              Sentry.captureException(err)
+            }
 
-              const timezone = user.timezone || DEFAULT_TIMEZONE
-              const startTime = dayjs(meeting.start).tz(timezone).format('h:mm A')
-              const endTime = dayjs(meeting.end).tz(timezone).format('h:mm A')
+            const timezone = user.timezone || DEFAULT_TIMEZONE
+            const startTime = dayjs(meeting.start).tz(timezone).format('h:mm A')
+            const endTime = dayjs(meeting.end).tz(timezone).format('h:mm A')
+
+            try {
+              debug(`sending email to ${email}...`)
 
               await mail.send(Templates.MeetingEnd, {
                 email: email,
@@ -143,7 +153,7 @@ export const OnPresenceDeleted = functions
             }
           }
 
-          debug('updating missed data in meeting')
+          debug('inserting infered user data in meeting')
 
           // Update values on the database
           await doc.set({ attendees: meeting.attendees, conversation: meeting.conversation }, { merge: true })
